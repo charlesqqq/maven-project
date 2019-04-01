3ea937aa0d45dc80fd9639c3ba0a7d6465093a5c
charlesqiu <zga12181991@gmail.com>
2019-04-01 09:18:09 +0800

    2

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -11,7 +11,12 @@ public class Person {
   public Person() {
 
   }
-
+	
+  /**
+   *
+   * @param someone someone
+   * @return hello
+   */
   final public String person(final String someone) {
     return String.format("Hello, %s!", someone);
   }
