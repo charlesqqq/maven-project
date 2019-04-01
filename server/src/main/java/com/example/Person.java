ba0005a20340eca196685066d6b43e0e649d844e
charlesqiu <zga12181991@gmail.com>
2019-04-01 09:14:16 +0800

    1

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -4,20 +4,15 @@ package com.example;
  * This is a class.
  */
 public class Person {
-	
-	/**
-	 * This is a constructor.
-	 */
-	public Person() {
-	
-	}
-	
-	/**
-	 *
-	 * @param someone someone
-	 * @return string
-	 */
-	final public String person(final String someone) {
-		return String.format("Hello, %s!", someone);
-	}
+
+  /**
+   * This is a constructor.
+   */
+  public Person() {
+
+  }
+
+  final public String person(final String someone) {
+    return String.format("Hello, %s!", someone);
+  }
 }
